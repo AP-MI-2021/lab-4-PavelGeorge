06a82882a5c89dcf1030ab13ec51a797091e1d8d
unknown <Sprynt3rGeo@gmail.com>
2021-10-20 19:11:30 +0300

    Am implementat a 2-a cerinta

--- main.py
+++ main.py
@@ -11,16 +11,38 @@ def citire_lista():
     return lst
 
 
+def numere_negative(lst):
+    """
+    Determina numerele negative nenule din lista
+    :param lst:Lista de numere
+    :return:Numerele negative din lst
+    """
+    rezultat = []
+    for x in lst:
+        if x < 0:
+            rezultat.append(x)
+    return rezultat
+
+
+def test_numere_negative():
+    assert numere_negative([5, 6, 10, 0]) == []
+    assert numere_negative([-5, -10, 6, 7]) == [-5, -10]
+
+
 def main():
     lst = []
+    test_numere_negative()
     while True:
         print("1. Citire lista")
+        print("2. Afisarea numerelor negative din lista")
         print("a. Afisare lista")
         print("x. Iesire")
 
         optiune = input("Dati optiunea: ")
         if optiune == "1":
             lst = citire_lista()
+        elif optiune == "2":
+            print(numere_negative(lst))
         elif optiune == "a":
             print(lst)
         elif optiune == "x":
