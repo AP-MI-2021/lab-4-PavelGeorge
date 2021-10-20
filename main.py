ea52b8924ea28014d9841ea9cec6bd79145a974b
unknown <Sprynt3rGeo@gmail.com>
2021-10-20 19:29:48 +0300

    Am implementat a 3-a cerinta

--- main.py
+++ main.py
@@ -29,12 +29,27 @@ def test_numere_negative():
     assert numere_negative([-5, -10, 6, 7]) == [-5, -10]
 
 
+def ultima_cifra(lst, cifra):
+    listacresc = lst[:]
+    listacresc.sort()
+    for x in listacresc:
+        if x % 10 == cifra:
+            return x
+
+
+def test_ultima_cifra():
+    assert ultima_cifra([1, 6, 34, 68, 40, 48, 20], 8) == 48
+    assert ultima_cifra([4, 10, 14, 28, 29, 24], 4) == 4
+
+
 def main():
     lst = []
     test_numere_negative()
+    test_ultima_cifra()
     while True:
         print("1. Citire lista")
         print("2. Afisarea numerelor negative din lista")
+        print("3. Afisarea celui mai mic numar care are ultima cifra egala cu un nr. dat")
         print("a. Afisare lista")
         print("x. Iesire")
 
@@ -43,6 +58,13 @@ def main():
             lst = citire_lista()
         elif optiune == "2":
             print(numere_negative(lst))
+        elif optiune == "3":
+            cifra = int(input("Dati un nr.: "))
+            minim = ultima_cifra(lst, cifra)
+            if minim is None:
+                print("Nu exista")
+            else:
+                print(minim)
         elif optiune == "a":
             print(lst)
         elif optiune == "x":
